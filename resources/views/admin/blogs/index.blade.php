89714a7b0e85043c722184a398c98387d35a76a4
mu7kata <mu7kata@icloud.com>
2024-05-16 23:13:23 +0900

    ブログ管理の文言にいたずらした

--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -4,10 +4,10 @@
 <section>
     <div class="mb-6 py-4 bg-white rounded">
         <div class="flex px-6 pb-4 border-b">
-            <h2 class="text-xl font-bold">ブログ一覧</h2>
+            <h2 class="text-xl font-bold">ブログ一asjdcnksdjnckjsdncksjdckjsdc覧</h2>
             <div class="ml-auto">
                 <a href="/admin/blogs/create"
-                    class="py-2 px-3 text-xs text-white font-semibold bg-indigo-500 rounded-md">新規投稿</a>
+                    class="py-2 px-3 text-xs text-white font-semibold bg-indigo-500 rounded-md">新aisdcbasjdcksdcsakdjnc規投稿</a>
             </div>
         </div>
         <div class="pt-4 px-4 overflow-x-auto">
