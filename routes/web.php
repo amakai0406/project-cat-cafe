ef1ed89b79a546122cc0c6de03d15e2ed97f67f8
あまかい0406 <amakaiamakai0406@gmail.com>
2024-06-05 09:27:47 +0900

    ユーザー編集画面の表示

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,9 @@ Route::prefix(('/admin'))
             Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
             Route::post('/users', [UserController::class, 'store'])->name('users.store');
 
+            Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+            Route::post('/users/{user}', [UserController::class, 'update'])->name('users.update');
+
             //ログアウト
             Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
         });
